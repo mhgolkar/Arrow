5bcb26cad71e268a87046af5f26c52646ba14295
M. H. Golkar <1233337+mhgolkar@users.noreply.github.com>
2024-03-01 16:34:34 +0330

    v2.3.0 with multiple UX improvements

--- v2/index.service.worker.js
+++ v2/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1693055340|3003033294";
+const CACHE_VERSION = "1709298052|105311230";
 const CACHE_PREFIX = "Arrow-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
