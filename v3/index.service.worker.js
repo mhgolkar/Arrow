6c73d80d60e41c1a9f4b6f07914de0545a742adf
M. H. Golkar <1233337+mhgolkar@users.noreply.github.com>
2025-07-30 11:02:10 +0330

    v3.1.0

--- v3/index.service.worker.js
+++ v3/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741180378|981674004';
+const CACHE_VERSION = '1753859910|216466351';
 /** @type {string} */
 const CACHE_PREFIX = 'Arrow-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
